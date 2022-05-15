4268541110b92025dd75b6ff7fe540eca194cff5
GeomJa <52377043+donut0310@users.noreply.github.com>
2022-05-15 10:41:34 +0900

    기능 구현 완료
    - Status: 각 대기장소에 대기하고 있는 배달 자동차의 정보 출력
    - Cancel: 해당 배달자동차를 대기장소에서 삭제
    - Deliver: 해당 대기장소에서 대기하고 있는 배달자동차 중에서 우선순위가 가장 높은 배달자동차를 배달 보내기
    - Clear: 해당 대기장소에서 대기하고 있는 배달자동차의 대기를 취소
    - Quit: 프로그램 종료

--- 과제 2/DeliveryVehicleManager/DeliveryVehicleManager.cs
+++ 과제 2/DeliveryVehicleManager/DeliveryVehicleManager.cs
@@ -25,71 +25,132 @@ class DeliveryVehicleManager {
         get {return this.numWaitingPlaces;}
     }
 
-    // Method
+    // --------------- Method --------------- //
+
+    // 배달자동차를 특정 대기장소에 배정하는 메소드
     public void ReadyIn(string _wp, string _vid, string _dest, string _p){
         int wp = int.Parse(_wp.Split('W')[1]); // 대기장소 번호
         int vid = int.Parse(_vid); // 배달 자동차 번호
         int p = int.Parse(_p.Split("P")[1]); // 배달 자동차 우선순위
         
+        // 배달 자동차 객체 생성
         DeliveryVehicle d = new DeliveryVehicle(vid, _dest, p);
 
-        // 배열크기 재조정
+        // 배열 크기 재조정
         int arrSize = this.waitPlaces[wp-1].Length;
         Array.Resize<DeliveryVehicle>(ref waitPlaces[wp-1], arrSize+1); 
+
         // 배달 자동차 배정
         waitPlaces[wp-1][arrSize] = d;
+
         // 배달 자동차의 우선순위에 따른 정렬
         Array.Sort(waitPlaces[wp-1], new PriorityComparer());
+
+        // 파일 출력
         StreamWriter sw = File.AppendText("OUTPUT.txt");
         sw.WriteLine("Vehicle {0} assigned to waitPlace #{1}", vid, wp);
         sw.Close();
     }
 
+    // 가장 적은 배달자동차가 대기하고 있는 대기장소에 배달 자동차 배정 메소드
     public void Ready(string _vid, string _dest, string _p){
         int vid = int.Parse(_vid); // 배달 자동차 번호
         int p = int.Parse(_p.Split("P")[1]); // 배달 자동차 
 
+        // 배달 자동차 객체 생성
         DeliveryVehicle d = new DeliveryVehicle(vid, _dest, p);
         
         // 가장 적은 배달자동차가 대기 중인 대기장소 선별
-        var max = (0,0);
-        for(int i=0;i<numWaitingPlaces;i++){
+        var min = (0,float.PositiveInfinity);
+        for(int i = 0; i < numWaitingPlaces; i++){
             int cnt = waitPlaces[i].Length;
-            if(cnt >= max.Item2){
+            if(cnt < min.Item2){
                 var tmp = (i, cnt);
-                max = tmp;
+                min = tmp;
             }
         }
 
         // 배열크기 재조정
-        int arrSize = this.waitPlaces[max.Item1].Length;
-        Array.Resize<DeliveryVehicle>(ref waitPlaces[max.Item1], arrSize+1); 
+        int arrSize = this.waitPlaces[min.Item1].Length;
+        Array.Resize<DeliveryVehicle>(ref waitPlaces[min.Item1], arrSize+1); 
 
         // 배달 자동차 배정
-        waitPlaces[max.Item1][arrSize] = d;
+        waitPlaces[min.Item1][arrSize] = d;
         
+        // 배달 자동차의 우선순위에 따른 정렬
+        Array.Sort(waitPlaces[min.Item1], new PriorityComparer());
+
+        // 파일 출력
         StreamWriter sw = File.AppendText("OUTPUT.txt");
-        sw.WriteLine("Vehicle {0} assigned to waitPlace #{1}", vid, max.Item1+1);
+        sw.WriteLine("Vehicle {0} assigned to waitPlace #{1}", vid, min.Item1+1);
         sw.Close();       
     }
 
+    // 각 대기장소에 대기하고 있는 배달 자동차의 정보 출력 메소드
     public void Status(){
+        StreamWriter sw = File.AppendText("OUTPUT.txt");
+        sw.WriteLine("************************ Delivery Vehicle Info ************************");
+        sw.WriteLine("Number of WaitPlaces: {0}", numWaitingPlaces);
+        for(int i = 0; i < numWaitingPlaces; i++){
+            sw.WriteLine("WaitPlace #{0} Number Vehicles: {1}", i+1, waitPlaces[i].Length);
+            if(waitPlaces[i].Length > 0){
+                foreach (DeliveryVehicle wp in waitPlaces[i])
+                    sw.WriteLine("FNUM: {0} DEST: {1} PRIO: {2}", wp.Vid, wp.Destination, wp.Priority);
+            }
+            sw.WriteLine("---------------------------------------------------");
+        }
+        sw.WriteLine("************************ End Delivery Vehicle Info ************************");
+        sw.Close();  
+    }
+
+    // 해당 배달자동차를 대기장소에서 삭제하는 메소드
+    public void Cancel(string _vid){
+        int vid = int.Parse(_vid); // 배달 자동차 번호
+        for(int i = 0; i < numWaitingPlaces; i++){
+            int indexToRemove = Array.FindIndex(waitPlaces[i], row => row.Vid == vid);
+            if(indexToRemove >= 0){
+                // 해당 배달 자동차 삭제 및 배열 사이즈 -1
+                waitPlaces[i] = waitPlaces[i].Where((source, index) => index != indexToRemove).ToArray();
+            }
+        }
+        // 파일 출력
+        StreamWriter sw = File.AppendText("OUTPUT.txt");
+        sw.WriteLine("Cancelation of Vehicle {0} completed.", vid);
+        sw.Close();
+    }
+
+    /* 해당 대기장소에서 대기하고 있는 배달자동차 중에서 우선순위가
+       가장 높은 배달자동차를 배달 보내는 메소드 */
+    public void Deliver(string _wp){
+        int wp = int.Parse(_wp.Split('W')[1]); // 대기장소 번호
+        int vid = waitPlaces[wp-1][0].Vid; // 배달 자동차 번호
+
+        /* 우선순위가 가장 높은 배달 자동차 => 배열 인덱스 0
+            대기장소에서 배달 자동차 객체 삭제 및 사이즈 재조정*/
+        waitPlaces[wp-1] = waitPlaces[wp-1].Where((source, index) => index != 0).ToArray();
+
+        // 파일 출력
+        StreamWriter sw = File.AppendText("OUTPUT.txt");
+        sw.WriteLine("Vehicle {0} used to deliver.", vid);
+        sw.Close();      
 
     }
 
-    public void Cancel(){
+    // 해당 대기장소에서 대기하고 있는 배달자동차의 대기를 취소하는 메소드
+    public void Clear(string _wp){
+        int wp = int.Parse(_wp.Split('W')[1]); // 대기장소 번호
+        // 배달자동차 객체 삭제 및 배열 사이즈 재조정
+        Array.Clear(waitPlaces[wp-1], 0, waitPlaces[wp-1].Length);
+        Array.Resize<DeliveryVehicle>(ref waitPlaces[wp-1], 0); 
 
-    }
-
-    public void Deliver(){
-
-    }
-
-    public void Clear(){
-
+        // 파일 출력
+        StreamWriter sw = File.AppendText("OUTPUT.txt");
+        sw.WriteLine("WaitPlace #{0} cleared.", wp);
+        sw.Close();
     }
 }
 
+// 배달 자동차의 우선순위에 따른 정렬
 class PriorityComparer : IComparer
 {
     public int Compare(object x, object y)
--- 과제 2/DeliveryVehicleManager/Program.cs
+++ 과제 2/DeliveryVehicleManager/Program.cs
@@ -3,11 +3,18 @@ using System.IO;
 
 class Program {
     static void Main(string[] args) {
+        // 초기화 => INPUT.txt 파일 위치 지정
         string path = String.Format("{0}/INPUT.txt", System.Environment.CurrentDirectory);
         StreamReader sr = new StreamReader(path);
         DeliveryVehicleManager dvm = new DeliveryVehicleManager(int.Parse(sr.ReadLine()));
-        int lineNum = 1;
 
+        //  초기화 => OUTPUT.txt 파일 존재시 삭제
+        string outputFile = String.Format("{0}/OUTPUT.txt", System.Environment.CurrentDirectory);
+        if(File.Exists(outputFile)){
+            File.Delete(outputFile);
+        }
+
+        int lineNum = 1;
         while(sr.Peek()>=0){
             if(lineNum==1){
                 lineNum+=1;
@@ -23,12 +30,16 @@ class Program {
                     dvm.Ready(tmp[1], tmp[2], tmp[3]);
                     break;
                 case "Status":
+                    dvm.Status();
                     break;
                 case "Cancel":
+                    dvm.Cancel(tmp[1]);
                     break;
                 case "Deliver":
+                    dvm.Deliver(tmp[1]);
                     break;
                 case "Clear":
+                    dvm.Clear(tmp[1]);
                     break;
                 case "Quit":
                     return;
